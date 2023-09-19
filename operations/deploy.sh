2478e33befee43c7d5580f93513e8c4fad29484d
Abigail Gutai <argutai@hotmail.com>
2023-09-19 10:20:40 +0100

    add operations script to update deployment to VM

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -7,8 +7,10 @@ sudo systemctl reload apache2
 
 query=$(curl https://localhost:443 --insecure)
 SUB='TCR Analysis Home'
+RED='\033[0;31m'; GREEN='\033[0;32m'; NC='\033[0m'
+
 if [[ "$query" == *"$SUB"* ]]; then 
-    Green='\033[0;32m'
-    NC='\033[0m'
-    echo -e "${Green}Deployment updated successfully.${NC}"
-fi
+    echo -e "${GREEN}Deployment updated successfully.${NC}"
+else
+    tail -n 5 /var/log/apache2/error.log
+    echo -e "${RED}Deployment update failed.${NC}"
